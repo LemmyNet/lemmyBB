e82d3f672f6114e2696b6b3f31c405326bf8359a
Felix Ableitner <me@nutomic.com>
2022-06-30 22:36:07 +0200

    Move route handlers to separate file

--- src/main.rs
+++ src/main.rs
@@ -3,76 +3,14 @@ extern crate rocket;
 
 mod api;
 mod error;
+mod routes;
 
-use crate::{
-    api::{create_site, get_post, get_site, list_posts, login, register, resolve_object},
-    error::ErrorPage,
-};
+use crate::api::{create_site, get_site, register, resolve_object};
 use anyhow::Error;
-use lemmy_api_common::{
-    lemmy_db_views::structs::{PostView, SiteView},
-    post::GetPostResponse,
-};
 use log::{info, LevelFilter};
-use rocket::{
-    form::Form,
-    fs::{relative, FileServer},
-    http::{Cookie, CookieJar},
-    response::Redirect,
-};
-use rocket_dyn_templates::{context, Template};
-use serde::Serialize;
-
-#[derive(Serialize)]
-struct ViewForumTemplate {
-    site: SiteView,
-    posts: Vec<PostView>,
-}
-
-#[get("/")]
-async fn view_forum() -> Result<Template, ErrorPage> {
-    let site = get_site().await?.site_view.unwrap();
-    let posts = list_posts().await?.posts;
-    let ctx = ViewForumTemplate { site, posts };
-    Ok(Template::render("viewforum", ctx))
-}
-
-#[derive(Serialize)]
-struct ViewTopicTemplate {
-    site: SiteView,
-    post: GetPostResponse,
-}
-
-#[get("/viewtopic?<t>")]
-async fn view_topic(t: i32) -> Result<Template, ErrorPage> {
-    let site = get_site().await?.site_view.unwrap();
-    let post = get_post(t).await?;
-    let ctx = ViewTopicTemplate { site, post };
-    Ok(Template::render("viewtopic", ctx))
-}
-
-#[get("/login")]
-async fn login_page() -> Result<Template, ErrorPage> {
-    let site = get_site().await?.site_view.unwrap();
-    Ok(Template::render("login", context!(site)))
-}
-
-#[derive(FromForm)]
-struct LoginForm {
-    username: String,
-    password: String,
-}
-
-#[post("/do_login", data = "<form>")]
-async fn do_login(form: Form<LoginForm>, cookies: &CookieJar<'_>) -> Result<Redirect, ErrorPage> {
-    let jwt = login(&form.username, &form.password)
-        .await?
-        .jwt
-        .unwrap()
-        .into_inner();
-    cookies.add(Cookie::new("jwt", jwt));
-    Ok(Redirect::to(uri!(view_forum)))
-}
+use rocket::fs::{relative, FileServer};
+use rocket_dyn_templates::Template;
+use routes::{do_login, login_page, view_forum, view_topic};
 
 async fn create_test_items() -> Result<(), Error> {
     //TODO: these usually fail with timeout, as http_fetch_retry_limit is reached
---
+++ src/routes.rs
@@ -0,0 +1,69 @@
+use crate::{
+    api::{get_post, get_site, list_posts, login},
+    error::ErrorPage,
+};
+use lemmy_api_common::{
+    lemmy_db_views::structs::{PostView, SiteView},
+    post::GetPostResponse,
+};
+use rocket::{
+    form::Form,
+    http::{Cookie, CookieJar},
+    response::Redirect,
+};
+use rocket_dyn_templates::{context, Template};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ViewForumTemplate {
+    site: SiteView,
+    posts: Vec<PostView>,
+}
+
+#[get("/")]
+pub async fn view_forum() -> Result<Template, ErrorPage> {
+    let site = get_site().await?.site_view.unwrap();
+    let posts = list_posts().await?.posts;
+    let ctx = ViewForumTemplate { site, posts };
+    Ok(Template::render("viewforum", ctx))
+}
+
+#[derive(Serialize)]
+struct ViewTopicTemplate {
+    site: SiteView,
+    post: GetPostResponse,
+}
+
+#[get("/viewtopic?<t>")]
+pub async fn view_topic(t: i32) -> Result<Template, ErrorPage> {
+    let site = get_site().await?.site_view.unwrap();
+    let post = get_post(t).await?;
+    let ctx = ViewTopicTemplate { site, post };
+    Ok(Template::render("viewtopic", ctx))
+}
+
+#[get("/login")]
+pub async fn login_page() -> Result<Template, ErrorPage> {
+    let site = get_site().await?.site_view.unwrap();
+    Ok(Template::render("login", context!(site)))
+}
+
+#[derive(FromForm)]
+pub struct LoginForm {
+    username: String,
+    password: String,
+}
+
+#[post("/do_login", data = "<form>")]
+pub async fn do_login(
+    form: Form<LoginForm>,
+    cookies: &CookieJar<'_>,
+) -> Result<Redirect, ErrorPage> {
+    let jwt = login(&form.username, &form.password)
+        .await?
+        .jwt
+        .unwrap()
+        .into_inner();
+    cookies.add(Cookie::new("jwt", jwt));
+    Ok(Redirect::to(uri!(view_forum)))
+}
