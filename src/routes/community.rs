77aa953de89c7130c920c8bc616c18c0406538a0
Felix Ableitner <me@nutomic.com>
2022-10-10 16:54:38 +0200

    Slightly improve display of instance/community sidebar

--- src/routes/community.rs
+++ src/routes/community.rs
@@ -1,5 +1,5 @@
 use crate::{
-    api::{post::list_posts, site::get_site_data},
+    api::{community::get_community, post::list_posts, site::get_site_data},
     routes::{auth, ErrorPage},
 };
 use rocket::http::CookieJar;
@@ -8,7 +8,9 @@ use rocket_dyn_templates::{context, Template};
 #[get("/viewforum?<f>")]
 pub async fn view_forum(f: i32, cookies: &CookieJar<'_>) -> Result<Template, ErrorPage> {
     let site_data = get_site_data(cookies).await?;
-    let posts = list_posts(f, 20, auth(cookies)).await?.posts;
+    let auth = auth(cookies);
+    let posts = list_posts(f, 20, auth.clone()).await?.posts;
+    let community = get_community(f, auth).await?;
     /*
     let last_replies = join_all(
         posts
@@ -19,6 +21,6 @@ pub async fn view_forum(f: i32, cookies: &CookieJar<'_>) -> Result<Template, Err
     .into_iter()
     .collect::<Result<Vec<PostOrComment>, Error>>()?;
     */
-    let ctx = context! { site_data, f, posts };
+    let ctx = context! { site_data, community, posts };
     Ok(Template::render("viewforum", ctx))
 }
